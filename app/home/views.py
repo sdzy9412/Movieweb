15e05b7097f52d6e26d26cb9b8eb94fe4a8099b3
YiZhang <56272051+sdzy9412@users.noreply.github.com>
2020-06-04 20:26:05 +0100

    user

--- app/home/views.py
+++ app/home/views.py
@@ -2,9 +2,6 @@
 from . import home
 from flask import render_template,redirect,url_for
 
-@home.route("/")
-def index():
-    return render_template("home/index.html")
 
 @home.route("/login/")
 def login():
@@ -13,3 +10,35 @@ def login():
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home.login"))
+
+@home.route("/regist/")
+def regist():
+    return render_template("home/regist.html")
+
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
+
+@home.route("/")
+def loginlog():
+    return render_template("home/index.html")
+
+@home.route("/animation/")
+def moviecol():
+    return render_template("home/animation.html")
